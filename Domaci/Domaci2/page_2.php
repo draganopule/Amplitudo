44ff2e9781a34644bc7e131f981b9c4cb576bba7
Dragan Jovanovic <dragan.jovanovic.me@gmail.com>
2019-11-02 13:53:48 +0100

    Echo list of GET parms from page_2.php

---
+++ Domaci/Domaci2/page_2.php
@@ -0,0 +1,6 @@
+<?php
+include('./helpers.php'); //ukljucili smo stranicu u kojoj se nalaze funkcije koje koristimo
+
+echo formatGetParams();//stampamo html listu svih GET parametara koji su proslijedjeni trenutnoj stranici
+
+?>
