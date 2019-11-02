368cf6f995ab46671595dda8581a08328a18e63f
Dragan Jovanovic <dragan.jovanovic.me@gmail.com>
2019-11-02 13:48:37 +0100

    Adds formatGetParams function in helpers.php

--- Domaci/Domaci2/helpers.php
+++ Domaci/Domaci2/helpers.php
@@ -20,3 +20,14 @@ function generateHref($pageName, $params)
 {
     return $pageName . "?" . generateUrlQuery($params);
 }
+
+//funkcija koja vraca html listu svih GET parametara koji su proslijedjeni trenutnoj stranici
+function formatGetParams()
+{
+    $html = '<ul>';
+    foreach($_GET as $key => $value){
+        $html = $html . '<li>' . $key . ': ' . $value . '</li>';
+    }
+    $html = $html . '</ul>';
+    return $html;
+}
