85b3411069421125c9349a0ff13f2b71e3bed76e
Dragan Jovanovic <dragan.jovanovic.me@gmail.com>
2019-11-02 13:58:00 +0100

    Adds array of strings and check if they are valid IP address

--- Domaci/Domaci2/page_1.php
+++ Domaci/Domaci2/page_1.php
@@ -3,6 +3,23 @@ include('./helpers.php'); //ukljucili smo stranicu u kojoj se nalaze funkcije ko
 
 echo formatGetParams();//stampamo html listu svih GET parametara koji su proslijedjeni trenutnoj stranici
 
+//niz Ip adresa koje ce biti prosljedjene funkciji za provjeru validnosti
+$testExamples = [
+    '82.0.45.112',
+    '10.192.168.255',
+    '4.256.12.78',
+    '1a.23.56.4'
+];
+
+//provjeravamo svaku adresu iz niza, da li je validna
+foreach($testExamples as $t){
+    if(validateIP($t)){
+        echo '</br>' . $t . ' je validna IP adresa</br>';
+    }
+    else{
+        echo '</br>' . $t . ' NIJE validna IP adresa</br>';
+    }
+}
 
 ?>
 
