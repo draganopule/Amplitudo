2b40f05eff013a7363ad796ef988fdd024c4ba4c
Dragan Jovanovic <dragan.jovanovic.me@gmail.com>
2019-11-18 14:47:20 +0100

    adds autoload.php

---
+++ Domaci/domaci_4/autoload.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once './interfejsi/HTMLRenderable.php';
+require_once './kolekcije/Collection.php';
+require_once './izuzeci/CollectionOverflowException.php';
+require_once './interfejsi/Iterator.php';
+require_once './kolekcije/Queue.php';
+require_once './iteratori/QueueIterator.php';
+require_once './kolekcije/Stack.php';
+require_once './iteratori/StackIterator.php';
+
+
+?>
