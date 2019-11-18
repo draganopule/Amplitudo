8f53f4b9afae654ba0879a835e8bc11fb266c83e
Dragan Jovanovic <dragan.jovanovic.me@gmail.com>
2019-11-18 14:49:23 +0100

    Adds index.php

--- Domaci/domaci_4/index.php
+++ Domaci/domaci_4/index.php
@@ -1,21 +1,55 @@
 <?php
 
+require_once './autoload.php';
+
+use Amplitudo\Domaci4\Queue;
+use Amplitudo\Domaci4\Stack;
+
 $items = ['a','b','c','d'];
-$element = 'x';
-array_splice($items, 4, 0, $element);
 
-echo '<pre>';
+$queue = new Queue();
+foreach($items as $item){
+    $queue->enqueue($item);//preko metode enqueue($item) dodajemo element po element u queue
+}
+echo '<h3>Queue elementi:</h3>';
+$queue->toHtml();//koristimo naslijedjenu metodu toHtml() da stampamo sve elemente iz kolekcije
 
-var_dump($items);
+echo '<h3>Queue iterator:</h3>';
+$queIterator = $queue->getIterator();//funkcijom getIterator() vracamo objekat klase QueueIterator
 
-echo '</pre>';
 
-echo array_shift($items);
+while($queIterator->hasNext()){//u while petlji koristimo metode tog objekta hasNext() 
+    echo '<p>' . $queIterator->next() . '</p>';//i next() da se krecemo kroz kolekciju i  
+}                                              //jedan po jedan element 'izvlacimo' iz kolekcije
 
-echo '<pre>';
+if($queue->isEmpty()){
+    echo '<h3>Nakon iteratora nema vise elemenata u kolekciji queue</h3>';
+}
+else{
+    echo '<h3>Nakon iteratora u kolekciji su ostali sledeci elementi:</h3>';
+    $queue->toHtml();
+}
 
-var_dump($items);
 
-echo '</pre>';
+$stack = new Stack();
+foreach($items as $item){
+    $stack->push($item);//preko metode push($item) dodajemo element po element u stack
+}
+echo '<h3>Stack elementi:</h3>';
+$stack->toHtml();//koristimo naslijedjenu metodu toHtml() da stampamo sve elemente iz kolekcije
+
+echo '<h3>Stack iterator:</h3>';
+$stackIterator = $stack->getIterator();//funkcijom getIterator() vracamo objekat klase StackIterator
+while($stackIterator->hasNext()){//u while petlji koristimo metode tog objekta hasNext() 
+    echo '<p>' . $stackIterator->next() . '</p>';//i next() da se krecemo kroz kolekciju i  
+}                                              //jedan po jedan element 'izvlacimo' iz kolekcije
+
+if($stack->isEmpty()){
+    echo '<h3>Nakon iteratora nema vise elemenata u kolekciji stack</h3>';
+}
+else{
+    echo '<h3>Nakon iteratora u kolekciji su ostali sledeci elementi:</h3>';
+    $stack->toHtml();
+}
 
 ?>
