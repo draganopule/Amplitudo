699dc8226bd0e255a0441bf37756d74aed8d2b60
Dragan Jovanovic <dragan.jovanovic.me@gmail.com>
2019-11-27 11:04:01 +0100

    Adds comment to BMWFactory.php

--- Domaci/domaci_5/Factory/BMWFactory.php
+++ Domaci/domaci_5/Factory/BMWFactory.php
@@ -2,8 +2,16 @@
 
 namespace Amplitudo\Domaci5\Factory;
 
+//Klasa BMWFactory sluzi za pravljenje automobila marke "BMW"
 class BMWFactory implements GeneralFactory
 {
+    /*
+    Funkcija create($model) kreira automobil pozivom konstruktora klase Automobile
+    i prosljedjuje mu kao nepromjenjive podatke o proizvodjacu i godini proizvodnje.
+    Godina proizvodnje se generise automatski pozivom ugradjene funkcije date('Y').
+    Podatak za model automobila je promjenjiv i on zavisi od toga sta je korisnik 
+    unio kao argument kad je pozvao funkciju create($model).
+    */
     public static function create($model)
     {
         return new Automobile('BMW', $model, date('Y'));
