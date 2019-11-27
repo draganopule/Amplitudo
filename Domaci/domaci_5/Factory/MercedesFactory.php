c9425b102a2838840e7274ae48d7c3ca90eccf69
Dragan Jovanovic <dragan.jovanovic.me@gmail.com>
2019-11-27 11:04:54 +0100

    Adds comment to MercedesFactory.php

--- Domaci/domaci_5/Factory/MercedesFactory.php
+++ Domaci/domaci_5/Factory/MercedesFactory.php
@@ -2,8 +2,16 @@
 
 namespace Amplitudo\Domaci5\Factory;
 
+//Klasa MercedesFactory sluzi za pravljenje automobila marke "Mercedes"
 class MercedesFactory implements GeneralFactory
 {
+    /*
+    Funkcija create($model) kreira automobil pozivom konstruktora klase Automobile
+    i prosljedjuje mu kao nepromjenjive podatke o proizvodjacu i godini proizvodnje.
+    Godina proizvodnje se generise automatski pozivom ugradjene funkcije date('Y').
+    Podatak za model automobila je promjenjiv i on zavisi od toga sta je korisnik 
+    unio kao argument kad je pozvao funkciju create($model).
+    */
     public static function create($model)
     {
         return new Automobile('Mercedes', $model, date('Y'));
