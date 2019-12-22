020f56eea201142a9916f4c918dbb3040c32a574
Dragan Jovanovic <dragan.jovanovic.me@gmail.com>
2019-12-22 22:13:34 +0100

    Remove some functions from BookRepository.php

--- Domaci/Bookstore/src/Book/Repositories/BookRepository.php
+++ Domaci/Bookstore/src/Book/Repositories/BookRepository.php
@@ -15,34 +15,10 @@ class BookRepository extends ObjectRepository
 {
     protected $tableName = 'books';
     protected $primaryKey = 'isbn';
+    
     public function __construct(MySQL $connection)
     {
         parent::__construct($connection);
         $this->transformer = new BookTransformer($connection);
     }
-    // public function findById($id)
-    // {
-    //     //$value = $this->transformer->transformField($id, $this->primaryKey);
-    //     $query = "SELECT * FROM $this->tableName WHERE $this->primaryKey = $id";
-    //     $result = $this->connection->query($query);
-    //     $item = $result->fetch_assoc();
-    //     if (!$item) {
-    //         throw new ItemNotFoundException();
-    //     }
-    //     $transformed = $this->transformer->transform($item);
-    //     mysqli_free_result($result);
-    //     return $transformed;
-    // }
-     public function insert($book)
-     {
-         $data = $this->transformer->transformToArray($book);
-         $columns = '(' . implode(', ', array_keys($data)) . ')';
-         $values = '(' . implode(', ', array_values($data)) . ')';
-         $query = "INSERT INTO $this->tableName $columns VALUES $values";
-         if (!$this->connection->query($query)) {
-             throw new ItemNotSavedException();
-         }
-         
-         return  $this->findById($book->getIsbn());
-     }
 }
