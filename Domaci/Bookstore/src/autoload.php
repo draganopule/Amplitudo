6415c8c0f3176c89ffe27cf8ad9070576eb187f2
Dragan Jovanovic <dragan.jovanovic.me@gmail.com>
2019-12-22 22:09:49 +0100

    Adds require function.php to autoload.php

--- Domaci/Bookstore/src/autoload.php
+++ Domaci/Bookstore/src/autoload.php
@@ -7,6 +7,8 @@ require_once './Library/Exceptions/ItemNotFoundException.php';
 require_once './Library/Exceptions/ItemNotSavedException.php';
 require_once './Library/Exceptions/ItemNotDeletedException.php';
 
+require_once './functions.php';
+
 require_once './Book/Models/Book.php';
 require_once './Book/Repositories/BookRepository.php';
 require_once './Book/Transformers/BookTransformer.php';
